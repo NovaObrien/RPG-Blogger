9463b71af3e0301f7a5e45135e39fecc0be3ceae
Nova (Tanner) O'Brien <tannerkobrien@gmail.com>
2021-02-14 16:36:17 -0700

    Updated publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
- // NOTE to host on github publicPath: '/YOUR-GITHUB-REPOSITORY-NAME/',
-  publicPath: '/',
+  // NOTE to host on github publicPath: '/YOUR-GITHUB-REPOSITORY-NAME/',
+  publicPath: 'https://github.com/NovaObrien',
   outputDir: 'docs',
   productionSourceMap: false
 }
