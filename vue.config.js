af3dd813855eb763bd12ff284bdca5c2237de805
Nova (Tanner) O'Brien <tannerkobrien@gmail.com>
2021-02-17 15:42:58 -0700

    TEst

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // NOTE to host on github publicPath: '/YOUR-GITHUB-REPOSITORY-NAME/',
-  publicPath: '/RPG-Blogger/blog',
+  publicPath: '/RPG-Blogger/index.html',
   outputDir: 'docs',
   productionSourceMap: false
 }
